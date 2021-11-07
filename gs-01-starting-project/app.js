c440eb7ade316d3a859efad74db3b51accc85867
igoryen <igor.yentaltsev@gmail.com>
2021-11-07 13:58:24 -0500

    remake using Vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,5 +1,18 @@
 // descriptive code
-
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
 
 // prescriptive code
 // const buttonEl = document.querySelector('button');
